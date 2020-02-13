f88fb3b97bc5e32c7fa7acb24b8c7350b3df0652
Kurt Krafft <kkrafft6609@gmail.com>
2020-02-13 11:54:28 -0600

    MVP

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,4 +4,5 @@ console.log('hey')
 
 mainDomEntries.createMainForm();
 eventsManager.addLogButtonEventListener();
+eventsManager.loadAllCArds();
 eventsManager.addDeleteAndEditButtonEventListener();
